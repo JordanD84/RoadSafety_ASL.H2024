baeede2a954857f6094608ba4134ee256d30d72c
Jordan Dionne <>
2024-02-13 17:45:45 -0500

    integrated land use

--- scripts/land_use.R
+++ scripts/land_use.R
@@ -1,7 +1,22 @@
-# Land use data: https://donnees.montreal.ca/dataset/schema-affectation-densite
+library(arrow) # parquet file format
+library(sf) # SF for spatial analysis
+library(tidyverse) # data analysis
 
-mat = st_intersects(data_sf, land_use, sparse = FALSE)
-rownames(mat) <- data$int_no
+# Land use data: https://donnees.montreal.ca/dataset/schema-affectation-densite
+land_use <- read_sf("./raw_data/grandes_affectations/GrAffectations.shp")
+
+# Cleaned intersections data
+intersections_sf <- 
+  read_parquet("./processed_data/data_final.parquet") |> 
+  # converting to sf object
+  st_as_sf(
+    coords = c("x", "y"), # coords takes longitude first
+    # Set our coordinate reference system to EPSG:32188,
+    crs = 32188
+  )
+
+mat = st_intersects(intersections_sf, land_use, sparse = FALSE)
+rownames(mat) <- intersections_sf$int_no
 colnames(mat) <- land_use$AFFECTATIO
 
 aug_land_use <- 
@@ -15,7 +30,7 @@ aug_land_use <-
   summarise(value = sum(value), .groups = "drop") |> 
   filter(value >= 1) |> 
   pivot_wider(names_from = name, values_from = value, values_fill = 0) |> 
-  right_join(select(data, int_no), by = "int_no") |> 
+  right_join(select(as_tibble(intersections_sf), int_no), by = "int_no") |> 
   mutate(across(everything(), ~replace_na(., 0))) |> 
   pivot_longer(!int_no) |> 
   filter(value == 1) |>  
@@ -26,3 +41,5 @@ aug_land_use <-
 # Combine grand_espace_vert_ou_récréation and conservation with dominante_résidentielle
 # grande_emprise_ou_grande_infrastructure_publique with industrie 
 # NA with something
+
+write_parquet(aug_land_use, "./processed_data/land_use.parquet")
